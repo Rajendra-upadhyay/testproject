892a32412f3962fb03949e36fc78633d41747502
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2023-03-16 20:09:26 +0530

    adding TestBillPayment.java and TestRecharge class for recharge and bill payment e2e test

---
+++ src/test/java/com/test/e2e/billpayment/TestBillPayment.java
@@ -0,0 +1,11 @@
+package com.test.e2e.billpayment;
+
+import org.testng.annotations.Test;
+
+public class TestBillPayment {
+
+    @Test
+    public void initiateBillPayment(){
+        System.out.println("initate first bill payment");
+    }
+}
---
+++ src/test/java/com/test/e2e/recharge/TestRecharge.java
@@ -0,0 +1,11 @@
+package com.test.e2e.recharge;
+
+import org.testng.annotations.Test;
+
+public class TestRecharge {
+
+    @Test
+    public void initiateRecharge(){
+        System.out.println("initiate recharge");
+    }
+}
